0ccdffe5d913bea1004bc2e90645c663e5df0e3a
alexandermedv <alexandermed@mail.ru>
2022-05-18 00:04:33 +0300

    task6

---
+++ element-search/popups/task.js
@@ -0,0 +1,26 @@
+modal_main = document.getElementById('modal_main')
+modal_main.className += " modal_active";
+console.log(modal_main.className)
+
+collection = document.querySelectorAll('.modal__close')
+close_times = collection[0]
+console.log(close_times)
+close_times.onclick = () => {
+    modal_main.className -= " modal_active";
+}
+
+modal_success = document.getElementById('modal_success')
+success = document.querySelector('.show-success')
+success.onclick = () => {
+    modal_success.className += " modal_active";
+}
+
+collection = document.querySelectorAll('.modal__close')
+console.log(collection)
+console.log(collection[2])
+
+close_times2 = collection[2]
+console.log(close_times2)
+close_times2.onclick = () => {
+    modal_success.className -= " modal_active";
+}
