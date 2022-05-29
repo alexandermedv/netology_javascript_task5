c0c61d38aa71eb67b8417a58e3ab40a2d2776eee
alexandermedv <alexandermed@mail.ru>
2022-05-30 02:03:21 +0300

    changes in 2 and 3 tasks

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,27 +1,71 @@
 
-menu = Array.from(document.querySelectorAll('.menu__item'))
-getItem = index => menu[index]
+sub_menu_list = Array.from(document.querySelectorAll('.menu_sub'))
+console.log('sub_menu_list =', sub_menu_list)
 
-for (i=0; i< menu.length; i++) {
-    const item = getItem(i);
+// for (i=0; i< sub_menu_list.length; i++) {
+//     console.log('i =', i)
+//     const link = sub_menu_list[i].closest('.menu__item').querySelector('.menu__link')
+//     console.log('link =', link)
+
+//     console.log('i =', i)
+
     
-    if (item.querySelector('.menu_sub')) {
-        menu_link = item.querySelector('.menu__link')
-        menu_sub = item.querySelector('.menu_sub')
-        menu_link.onclick = () => {
-            if (menu_sub.className.includes(" menu_active")) {
-                menu_sub.className -= " menu_active";
+//     submenu = sub_menu_list[i]
+    
+//     link.onclick = () => {
+//         console.log('i =', i)
+
+//         let open = document.querySelector(".menu_active")
+
+//         if (open) {
+//             open.className = open.className.replace(" menu_active", "")
+
+//             if (submenu.closest('.menu__item').querySelector('.menu__link').textContent !== open.closest('.menu__item').querySelector('.menu__link').textContent) {
+//                 submenu.className += " menu_active";
+//             }
+//         } else {
+//             console.log('i =', i)
+//             console.log('sub_menu_list[i] =', submenu)
+//             submenu.className += " menu_active";
+//         }
+
+//         return false
+//     }
+// }
+
+link1 = sub_menu_list[0].closest('.menu__item').querySelector('.menu__link')
+link2 = sub_menu_list[1].closest('.menu__item').querySelector('.menu__link')
+
+link1.onclick = () => {
+
+            let open = document.querySelector(".menu_active")
+
+            if (open) {
+                open.className = open.className.replace(" menu_active", "")
+
+                if (sub_menu_list[0].closest('.menu__item').querySelector('.menu__link').textContent !== open.closest('.menu__item').querySelector('.menu__link').textContent) {
+                sub_menu_list[0].className += " menu_active";
+                }
             } else {
-               menu_sub.className += " menu_active"; 
+                sub_menu_list[0].className += " menu_active";
             }
+
+            return false
+        }
+    
+link2.onclick = () => {
+
+    let open = document.querySelector(".menu_active")
+
+    if (open) {
+        open.className = open.className.replace(" menu_active", "")
+
+        if (sub_menu_list[1].closest('.menu__item').querySelector('.menu__link').textContent !== open.closest('.menu__item').querySelector('.menu__link').textContent) {
+        sub_menu_list[1].className += " menu_active";
         }
     } else {
-        submenu_points = item.querySelectorAll('.menu__link')
-        for (j=0; j<submenu_points.length; j++) {
-            submenu_points[j].onclick = () => {
-                return false
-            }
-        }
+        sub_menu_list[1].className += " menu_active";
     }
-}
 
+    return false
+}
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,34 +1,21 @@
 slides = Array.from(document.querySelectorAll('.slider__item'))
-current = 0;
 
 prev = document.querySelector('.slider__arrow_prev')
 prev.onclick = () => {
-    if (current===0) {
-        prev_number = slides.length-1;
-    } else {
-        prev_number = current-1;
-    }
 
-    slides[current].className -= ' slider__item_active';
-    slides[current].style.display = 'none';
+    current = slides.findIndex((element) => {return element.className.includes('slider__item_active')})
+    prev_number = (current===0) ? prev_number = slides.length-1 : current-1;
+
+    slides[current].className = slides[current].className.replace(" slider__item_active", "")
     slides[prev_number].className += ' slider__item_active';
-    slides[prev_number].style.display = 'block';
-    
-    current = prev_number;
 }
 
 next = document.querySelector('.slider__arrow_next')
 next.onclick = () => {
-    if (current===slides.length-1) {
-        next_number = 0;
-    } else {
-        next_number = current+1;
-    }
 
-    slides[current].className -= ' slider__item_active';
-    slides[current].style.display = 'none';
+    current = slides.findIndex((element) => {return element.className.includes('slider__item_active')})
+    next_number = (current===slides.length-1) ? next_number = 0 : next_number = current+1;
+
+    slides[current].className = slides[current].className.replace(" slider__item_active", "")
     slides[next_number].className += ' slider__item_active';
-    slides[next_number].style.display = 'block';
-
-    current = next_number;
 }
